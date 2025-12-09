2abf365cdd86079cc56be3f822efca9dca587c25
Ajeet Kumar <ajeet@devseccops.ai>
2025-12-09 12:42:34 +0530

    fixed

--- generalscaler/main.py
+++ generalscaler/main.py
@@ -10,7 +10,7 @@ def main():
         format="%(asctime)s [%(levelname)s] %(name)s - %(message)s",
     )
     # logging.basicConfig(level=logging.INFO)
-    # kopf.run(standalone=True)
+    kopf.run(standalone=True)
 
 
 if __name__ == "__main__":
